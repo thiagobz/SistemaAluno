c9b5a31ce0d851f499c1973fe9364b6585b4fd4c
Thiago Bazani <tgobzrj@live.com>
2021-12-09 19:36:04 -0300

    Adicionado aquivo do sistema

---
+++ sistema.js
@@ -0,0 +1,26 @@
+let numeroDeAlunos = 10;
+
+for (let contador = 0; numeroDeAlunos >= contador; contador++) {
+  // console.log(contador)
+
+  if (contador == 0) {
+    console.log('O numero atual é zero')
+  } else if (contador % 2 == 0) {
+    console.log('O numero ' + contador + ' é par');
+  } else {
+    console.log(`O numero ${contador} é impar`);
+  }
+}
+
+
+let contador = 0
+while (contador <= numeroDeAlunos) {
+  if (contador == 0) {
+    console.log('O numero atual é zero')
+  } else if (contador % 2 == 0) {
+    console.log('O numero ' + contador + ' é par');
+  } else {
+    console.log(`O numero ${contador} é impar`);
+  }
+  contador++
+}
