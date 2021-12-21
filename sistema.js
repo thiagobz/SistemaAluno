9ed40d01b069e6c45d0cd8bf12cff63db78aa162
Thiago Bazani <50850641+thiagobz@users.noreply.github.com>
2021-12-21 12:10:58 -0300

    Update sistema.js

--- sistema.js
+++ sistema.js
@@ -1,26 +1,32 @@
-let numeroDeAlunos = 10;
+// let numeroDeAlunos = 10;
 
-for (let contador = 0; numeroDeAlunos >= contador; contador++) {
-  // console.log(contador)
+// for (let contador = 0; numeroDeAlunos >= contador; contador++) {
+// console.log(contador)
 
-  if (contador == 0) {
-    console.log('O numero atual é zero')
-  } else if (contador % 2 == 0) {
-    console.log('O numero ' + contador + ' é par');
-  } else {
-    console.log(`O numero ${contador} é impar`);
-  }
-}
+//   if (contador == 0) {
+//     console.log('O numero atual é zero')
+//   } else if (contador % 2 == 0) {
+//     console.log('O numero ' + contador + ' é par');
+//   } else {
+//     console.log(`O numero ${contador} é impar`);
+//   }
+// }
 
 
-let contador = 0
-while (contador <= numeroDeAlunos) {
-  if (contador == 0) {
-    console.log('O numero atual é zero')
-  } else if (contador % 2 == 0) {
-    console.log('O numero ' + contador + ' é par');
-  } else {
-    console.log(`O numero ${contador} é impar`);
-  }
-  contador++
+// let contador = 0
+// while (contador <= numeroDeAlunos) {
+//   if (contador == 0) {
+//     console.log('O numero atual é zero')
+//   } else if (contador % 2 == 0) {
+//     console.log('O numero ' + contador + ' é par');
+//   } else {
+//     console.log(`O numero ${contador} é impar`);
+//   }
+//   contador++
+// }
+
+let listaAlunos = ["Marcel", "Franklin", "Rita", "Afrânio"]
+
+for (let aluno of listaAlunos) {
+    console.log(`Este aluno se chama ${aluno}`)
 }
